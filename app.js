d346dac28bda56e4c2f38ba197e84a9a3ab443b8
MWyssy <mike@wyss.co.uk>
2023-04-25 18:47:23 +0300

    add challenges 7-11

--- app.js
+++ app.js
@@ -4,14 +4,39 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/owners/:id', (req, res) => {
+function logMethod(req, res, next) {
+    console.log('Request Type:', req.method)
+    const dataToWrite = `Request Type: ${req.method} \n`
+    fs.appendFile('./log.txt', dataToWrite).then(() => {
+        next()
+    })
+}
+function logUrl(req, res, next) {
+    console.log('Requested Url', req.url)
+    const dataToWrite = `Requested Url: ${req.url} \n`
+    fs.appendFile('./log.txt', dataToWrite).then(() => {
+        next()
+    })
+}
+
+function logRequestTime(req, res, next) {
+    console.log('Time of Request:', new Date(Date.now()).toString())
+    const dataToWrite = `Time of Request ${new Date(Date.now()).toString()} \n\n`
+    fs.appendFile('./log.txt', dataToWrite).then(() => {
+        next()
+    })
+} 
+
+const logStuff = [ logMethod, logUrl, logRequestTime ]
+
+app.get('/owners/:id', logStuff, (req, res, next) => {
     fs.readFile(`./data/owners/o${req.params.id}.json`, 'utf8').then((data) => {
         const parsedData = JSON.parse(data);
         res.send({ owner: parsedData })        
     })
 })  
 
-app.get('/owners', (req, res) => {
+app.get('/owners', logStuff, (req, res, next) => {
     const ownersArr = [];
     fs.readdir('./data/owners').then((data) => {
         data.forEach((owner) => {
@@ -25,7 +50,7 @@ app.get('/owners', (req, res) => {
         })
     })
 })
-app.get('/owners/:id/pets', (req, res) => {
+app.get('/owners/:id/pets', logStuff, (req, res, next) => {
     const petsArr = [];
     fs.readdir('./data/pets').then((files) => {
         files.forEach((pet) => {
@@ -43,7 +68,7 @@ app.get('/owners/:id/pets', (req, res) => {
     })
 })
 
-app.get('/pets', (req, res) => {
+app.get('/pets', logStuff, (req, res, next) => {
     const petsArr = [];
     console.log(req.query)
     const q = req.query
@@ -77,7 +102,7 @@ app.get('/pets', (req, res) => {
 })
 
 
-app.get('/pets/:id', (req, res) => {
+app.get('/pets/:id', logStuff, (req, res, next) => {
     fs.readFile(`./data/pets/p${req.params.id}.json`, 'utf8').then((data) => {
         const parsedData = JSON.parse(data);
         res.send({ pet: parsedData })        
@@ -87,30 +112,9 @@ app.get('/pets/:id', (req, res) => {
 
 
 app.patch('/owners/:id',(req,res)=>{
-//console.log(req.body)
 const ownerId=req.params.id;
 const parsedBody= req.body
-const keys=Object.keys(parsedBody)
-console.log(keys)
-// const truekey= keys.every((key=>{
-//     console.log(key)
-//     console.log(key === "name")
-//     !(key === "name")
-// }))
 
-
-
-if(!(parsedBody.hasOwnProperty("name") || parsedBody.hasOwnProperty("age"))){
-    //console.log('false')
-    res.status(406).send('error: only name and age value can be edited')
-} 
-
-
-else{
-   
-
-    // console.log(keys)
-    // console.log(values)
     fs.readFile(`./data/owners/o${ownerId}.json`, 'utf8').then((data) => {
         const parsedData = JSON.parse(data);
         console.log(parsedData)
@@ -127,11 +131,68 @@ else{
         res.status(200).send({owner:updatedOwnerData});
 
     })
-
-    // console.log("true")
-}
-
 })
 
+app.post('/owners', logStuff, (req, res, next) => {
+    const parsedBody = req.body;
+    const idNum = Date.now()
+    const newOwner = { id: `o${idNum}`, ...parsedBody}
+
+    fs.writeFile(`./data/owners/o${idNum}.json`, JSON.stringify(newOwner, null, 2)).then(() => {
+        res.status(200).send({ owner: newOwner });
+    })
+})
+
+app.post('/owners/:id/pets', logStuff, (req, res, next) => {
+    const id = req.params.id
+    const idNum = Date.now()
+    const parsedBody = req.body;
+    const newPet = { id: `p${idNum}`, owner: id, ...parsedBody }
+
+    fs.readdir('./data/owners').then((owners) => {
+        if (owners.includes(`${id}.json`)) {
+            return Promise.all([
+                newPet,
+                fs.writeFile(`./data/pets/p${idNum}.json`, JSON.stringify(newPet, null, 2))
+            ])
+        };
+    })
+    .then((data) => {
+        res.status(200).send({ pet: data[0] })
+    })
+})
+
+app.delete('/pets/:id', logStuff, (req, res, next) => {
+    const id = req.params.id;
+
+    fs.rm(`./data/pets/p${id}.json`).then(() => {
+        res.status(200).send(`p${id} successfully deleted`)
+    })
+})
+
+app.delete('/owners/:id', logStuff, (req, res, next) => {
+    const id = req.params.id;
+
+    fs.readdir('./data/pets').then((data) => {
+        data.forEach((pets) => {
+            fs.readFile(`./data/pets/${pets}`)
+            .then((petsData) => {
+                const pet = JSON.parse(petsData)
+                return pet
+            })
+            .then((pet) => {
+                if (pet.owner === `o${id}`) {
+                    fs.rm(`./data/pets/${pet.id}.json`)
+                }
+            })
+        })
+    })
+    .then(() => {
+        fs.rm(`./data/owners/o${id}.json`)
+    })
+    .then(() => {
+        res.status(200).send(`o${id} and their pets and have been successfully deleted`)
+    })
+}) 
 
 app.listen(9090, () => console.log('App listening on port 9090!'));
