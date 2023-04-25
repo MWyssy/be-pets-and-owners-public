3518dc0703eabe11838ccdb11c10a51a26409d97
MWyssy <mike@wyss.co.uk>
2023-04-25 12:51:24 +0300

    add challenge-1 solution

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const fs = require('fs/promises');
 const app = express();
 
 app.use(express.json());
 
+app.get('/owners/:id', (req, res) => {
+    fs.readFile(`./data/owners/o${req.params.id}.json`, 'utf8').then((data) => {
+        console.log(data)
+        const parsedData = JSON.parse(data);
+        res.send({ owner: parsedData })        
+    })
+})  
+
+app.listen(9090, () => console.log('App listening on port 9090!'));
